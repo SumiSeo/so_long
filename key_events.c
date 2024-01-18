71cb6e6d765d86707895be05486d47358dd4bd4a
Sumi Seo <sumseo@bess-f3r2s5.clusters.42paris.fr>
2024-01-18 16:15:12 +0100

    eat collectible

--- key_events.c
+++ key_events.c
@@ -3,53 +3,61 @@
 void	move_up(t_data *env)
 {
 	find_cur_position(env);
-	printf("up !! lessss go!!\n ");
 	if (env->position[env->cur_y - 1][env->cur_x] == '0')
 	{
 		env->position[env->cur_y - 1][env->cur_x] = 'P';
 		env->position[env->cur_y][env->cur_x] = '0';
-		display_game_to_window(env);
 	}
-	printf("Currently I am here i : %d, j : %d\n", env->cur_x, env->cur_y);
+	else if (env->position[env->cur_y - 1][env->cur_x] == 'C')
+	{
+		env->position[env->cur_y - 1][env->cur_x] = '0';
+	}
+	display_game_to_window(env);
 }
 
 void	move_right(t_data *env)
 {
 	find_cur_position(env);
-	printf("up !! lessss go!!\n ");
 	if (env->position[env->cur_y][env->cur_x + 1] == '0')
 	{
 		env->position[env->cur_y][env->cur_x + 1] = 'P';
 		env->position[env->cur_y][env->cur_x] = '0';
-		display_game_to_window(env);
 	}
-	printf("Currently I am here i : %d, j : %d\n", env->cur_x, env->cur_y);
+	else if (env->position[env->cur_y][env->cur_x + 1] == 'C')
+	{
+		env->position[env->cur_y][env->cur_x + 1] = '0';
+	}
+	display_game_to_window(env);
 }
 
 void	move_left(t_data *env)
 {
 	find_cur_position(env);
-	printf("up !! lessss go!!\n ");
 	if (env->position[env->cur_y][env->cur_x - 1] == '0')
 	{
 		env->position[env->cur_y][env->cur_x - 1] = 'P';
 		env->position[env->cur_y][env->cur_x] = '0';
-		display_game_to_window(env);
 	}
-	printf("Currently I am here i : %d, j : %d\n", env->cur_x, env->cur_y);
+	else if (env->position[env->cur_y][env->cur_x - 1] == 'C')
+	{
+		env->position[env->cur_y][env->cur_x - 1] = '0';
+	}
+	display_game_to_window(env);
 }
 
 void	move_down(t_data *env)
 {
 	find_cur_position(env);
-	printf("up !! lessss go!!\n ");
 	if (env->position[env->cur_y + 1][env->cur_x] == '0')
 	{
 		env->position[env->cur_y + 1][env->cur_x] = 'P';
 		env->position[env->cur_y][env->cur_x] = '0';
-		display_game_to_window(env);
 	}
-	printf("Currently I am here i : %d, j : %d\n", env->cur_x, env->cur_y);
+	else if (env->position[env->cur_y + 1][env->cur_x] == 'C')
+	{
+		env->position[env->cur_y + 1][env->cur_x] = '0';
+	}
+	display_game_to_window(env);
 }
 
 int	key_press(int keycode, t_data *env)
